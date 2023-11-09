b0be8d62d107a59124d00d350f55d1d11ceba449
Artashes <artashes.badalyan.999@gmail.com>
2023-11-09 19:38:42 +0400

    Mobile and Desktop

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,11 @@
 const { defineConfig } = require("cypress");
 
+
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       
     },
-    viewportHeight: 700,
-    viewportHeight: 650,
     experimentalStudio: true,
   },
 });
--- cypress/e2e/task1.cy.js
+++ cypress/e2e/task1.cy.js
@@ -1,26 +1,44 @@
 import testData from "../fixtures/test_datas"
 import loginPage  from "../pages/loginPage"
 
-const viewports = [
-  { width: 1200, height: 800 },  
-  { width: 375, height: 667 }
-];
+let deviceName = '';
 
-describe('Check Successfull login', () => {
-    viewports.forEach(viewport => {
-      
-      it('Should login successfully', () => {
-        cy.viewport(viewport.width,viewport.height)
+
+
+describe('Check Successfull login', () => { 
+   
+  beforeEach(() => {
+    if (Cypress.env('deviceName') === 'mobile') {
+      deviceName = 'iphone-8';
+    } else if (Cypress.env('deviceName') === 'desktop') {
+      deviceName = 'macbook-15';
+    }
+
+    // Set the viewport only once for all 'it' blocks
+    cy.viewport(deviceName);
+  });
+    
+    it('Should login successfully', () => {
         cy.visit(testData.mainData.baseUrl)
         loginPage.login(testData.loginData.username,testData.loginData.password)
         cy.url().should('include',testData.mainData.urlAfterLogin)
         cy.get(loginPage.elements.shopCart).should('be.visible')
     })
-    })
+    it('Should receive invalid credentials error message', () => {
+        cy.visit(testData.mainData.baseUrl)
+        loginPage.login(testData.loginData.invalidUsername,testData.loginData.invalidPswd)
+        cy.get(loginPage.elements.loginErrMessage).should('have.text', testData.loginData.logineErrMsg);
+  })
+    it('Should receive locked out user error message', () => {
+        cy.visit(testData.mainData.baseUrl)
+        loginPage.login(testData.loginData.lockedUsername,testData.loginData.password)
+        cy.get(loginPage.elements.loginErrMessage).should('have.text', testData.loginData.lockedAccountErr);
+    
+})
     
 })
 
-describe('Check Invalid message', () => {
+/*describe('Check Invalid message', () => {
   viewports.forEach(viewport => {
     it('Should receive invalid credentials error message', () => {
         cy.viewport(viewport.width,viewport.height)
@@ -38,7 +56,8 @@ describe('Check Locked account', () => {
         cy.visit(testData.mainData.baseUrl)
         loginPage.login(testData.loginData.lockedUsername,testData.loginData.password)
         cy.get(loginPage.elements.loginErrMessage).should('have.text', testData.loginData.lockedAccountErr);
+        
     })
   })
 })
-
+*/
