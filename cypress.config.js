928e2141b27915848f7a59951afc40b6a8aef297
Artashes <artashes.badalyan.999@gmail.com>
2023-11-13 18:11:27 +0400

    Api Testing

--- cypress.config.js
+++ cypress.config.js
@@ -6,6 +6,7 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       
     },
+    video: true,
     experimentalStudio: true,
   },
 });
---
+++ cypress/e2e/loremIpsum.cy.js
@@ -0,0 +1,19 @@
+const cheerio = require('cheerio');
+
+it('Should have h1 and "Lorem Ipsum" text', () => {
+  cy.request({
+    method: 'GET',
+    url: 'https://www.lipsum.com/',
+    headers: {
+      'Content-Type': 'text/html; charset=utf-8',
+    },
+  }).as('resp').then((response) => {
+    console.log('Response:', response);
+    expect(response.status).to.eq(200);
+    expect(response.headers['content-type']).to.include('text/html');
+    const $ = cheerio.load(response.body);
+    expect($('h1:contains("Lorem Ipsum")')).to.exist;
+  });
+});
+
+  
